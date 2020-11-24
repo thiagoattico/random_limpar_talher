3f467eaafc8c45c25ceea5845ea68a5535bb5c38
Thiago Attico <thiagoattico@hotmail.com>
2020-11-24 10:34:12 -0300

    Add files via upload

---
+++ SorteadorDeLimparTalher/Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace SorteadorDeLimparTalher
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnGeraNumeros = new System.Windows.Forms.Button();
+            this.btnLimpar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnGeraNumeros
+            // 
+            this.btnGeraNumeros.Location = new System.Drawing.Point(47, 159);
+            this.btnGeraNumeros.Name = "btnGeraNumeros";
+            this.btnGeraNumeros.Size = new System.Drawing.Size(124, 23);
+            this.btnGeraNumeros.TabIndex = 1;
+            this.btnGeraNumeros.Text = "Gerar Numeros";
+            this.btnGeraNumeros.UseVisualStyleBackColor = true;
+            this.btnGeraNumeros.Click += new System.EventHandler(this.btnGeraNumeros_Click);
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.Location = new System.Drawing.Point(243, 159);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(124, 23);
+            this.btnLimpar.TabIndex = 2;
+            this.btnLimpar.Text = "Limpar";
+            this.btnLimpar.UseVisualStyleBackColor = true;
+            this.btnLimpar.Click += new System.EventHandler(this.btnLimpar_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(103, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(202, 24);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Sorteador de Números";
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.AutoSize = true;
+            this.lblResultado.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResultado.Location = new System.Drawing.Point(117, 96);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(164, 20);
+            this.lblResultado.TabIndex = 5;
+            this.lblResultado.Text = "Número Sorteado foi: ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(410, 235);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnLimpar);
+            this.Controls.Add(this.btnGeraNumeros);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Sorteador de Lavar os Talheres";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnGeraNumeros;
+        private System.Windows.Forms.Button btnLimpar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblResultado;
+    }
+}
+
---
+++ SorteadorDeLimparTalher/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SorteadorDeLimparTalher
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGeraNumeros_Click(object sender, EventArgs e)
+        {
+            int num = 0;
+            MessageBox.Show("Lista de Pessoas: \n  \n 1- Thiago  \n 2- Will \n 3- Leonardo \n 4- Rubão \n 5-Claudio \n 6- Gusta \n 7- Diego");
+            Random randNum = new Random();
+            for (int i = 1; i < 2; i++)
+            num = randNum.Next(1, 8);
+
+            if (num == 1)
+            {
+                lblResultado.Text = "Número Sorteado foi: " + num;
+                MessageBox.Show("Thiago vai lavar");
+            }
+            else if (num == 2)
+            {
+                lblResultado.Text = "Número Sorteado foi: " + num;
+                MessageBox.Show("Will vai lavar");
+            }
+            else if (num == 3)
+            {
+                lblResultado.Text = "Número Sorteado foi: " + num;
+                MessageBox.Show("Leonardo vai lavar");
+            }
+            else if (num == 4)
+            {
+                lblResultado.Text = "Número Sorteado foi: " + num;
+                MessageBox.Show("Rubão vai lavar");
+            }
+            else if (num == 5)
+            {
+                lblResultado.Text = "Número Sorteado foi: " + num;
+                MessageBox.Show("Claudio vai lavar");
+            }
+            else if (num == 6)
+            {
+                lblResultado.Text = "Número Sorteado foi: " + num;
+                MessageBox.Show("Gusta vai lavar");
+            }
+            else
+            {
+                lblResultado.Text = "Número Sorteado foi: " + num;
+                MessageBox.Show("Diego vai lavar");
+            }
+        }
+
+        private void btnLimpar_Click(object sender, EventArgs e)
+        {
+            lblResultado.Text = "Número Sorteado foi: ";
+        }
+    }
+}
